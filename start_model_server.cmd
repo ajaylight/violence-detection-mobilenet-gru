31e0eef0e8f3b96349904022aa4d9ad37a76653a
Ajay Prakash <ajaylightyagami@gmail.com>
2026-06-21 16:20:17 +0530

    Update start_model_server.cmd

--- start_model_server.cmd
+++ start_model_server.cmd
@@ -1,1 +1,8 @@
+@echo off
+setlocal
 
+cd /d "%~dp0"
+
+if not exist "logs" mkdir "logs"
+
+"C:\vd310\Scripts\python.exe" ".\training\model_server.py" 1>> ".\logs\model-server.out.log" 2>> ".\logs\model-server.err.log"
